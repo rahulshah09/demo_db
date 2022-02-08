8d58a47e4352ede244edc0e8a300bd177885f2f8
wz00771 <wz00771@umbc.edu>
2022-02-08 14:26:41 +0530

    commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const express = require('express');
 const dotenv = require('dotenv');
+const multer = require('multer');
 
 dotenv.config({path: '.env-local'});
 
@@ -8,6 +9,30 @@ const PORT = process.env.PORT || '3000';
 
 const app = express();
 
+const fileStorageEngine = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '--' + file.originalname)
+    },
+});
+
+const upload = multer({ storage: fileStorageEngine });
+/*
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+*/
+app.post("/single", upload.single("image"), (req, res) => {
+    console.log(req.file);
+    res.send("Single File upload success");
+});
+
+app.post("/multiple", upload.array("images", 3),(req, res) => {
+    console.log(req.files);
+    res.send("Multiple Files Upload Success");
+});
 
 // Middleware
 
